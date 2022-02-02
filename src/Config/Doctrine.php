4236f4f99fa371d552f063b45ad40b71335dfd2d
Daycry <daycry9@gmail.com>
2022-02-02 17:18:18 +0100

    Update Doctrine.php

--- src/Config/Doctrine.php
+++ src/Config/Doctrine.php
@@ -11,12 +11,14 @@ class Doctrine extends BaseConfig
     // see doc https://www.doctrine-project.org/projects/doctrine-orm/en/2.8/reference/advanced-configuration.html#auto-generating-proxy-classes-optional
     public $setAutoGenerateProxyClasses = ENVIRONMENT === 'development' ? true : false;
 
-    /*
+    /* @deprecated
+     *
      * Methods "Redis, Memcached" or "null"
      */
     public $cache = null;
 
-    /*
+    /* @deprecated
+     *
      * Ports
      * for Redis : 6379
      * for Memcached : 11211
@@ -25,12 +27,14 @@ class Doctrine extends BaseConfig
 
     public $hostCache = null;
 
-    /*
+    /* @deprecated
+     *
      * Index of redis database
      */
     public $databaseRedis = 0;
 
-    /*
+    /* @deprecated
+     *
      * Namespace of Redis database or name of Memcached server
      */
     public $namespaceCache = "name";
